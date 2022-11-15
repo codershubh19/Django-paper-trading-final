1639d4f339431fce0a8bf84f238cfd22bd56f0da
codershubh19 <104767977+codershubh19@users.noreply.github.com>
2022-11-15 11:50:22 +0530

    final

--- Cryptic/trader/admin.py
+++ Cryptic/trader/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trader
+from .models import Trader,Transaction
 # Register your models here.
 
 admin.site.register(Trader)
+admin.site.register(Transaction)
